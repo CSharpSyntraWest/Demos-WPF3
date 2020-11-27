c1e1c5b6ae54d2f1214c5b87d4718ee8e59c1a8c
hcopp <hcopp@DESKTOP-SF67LSQ>
2020-11-27 13:33:07 +0100

    Demo Custom Date Converter

---
+++ Demos-controlsEnBinding/Utility/DateConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Demos_controlsEnBinding.Utility
+{
+    [ValueConversion(typeof(DateTime), typeof(String))]
+    public class DateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string returnValue = String.Empty;
+            if (value != null)
+            {
+                returnValue = ((DateTime)value).ToString("dd/MM/yyyy");
+            }
+
+            return returnValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime resultDateTime = DateTime.Parse(value.ToString());
+            return resultDateTime;
+        }
+    }
+}
---
+++ Demos-controlsEnBinding/Views/DemoCustomConverter.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demos_controlsEnBinding.Views
+{
+    /// <summary>
+    /// Interaction logic for DemoCustomConverter.xaml
+    /// </summary>
+    public partial class DemoCustomConverter : UserControl
+    {
+        public DemoCustomConverter()
+        {
+            InitializeComponent();
+           // Thread.CurrentThread.CurrentCulture = new CultureInfo("nl-BE");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("nl-BE");
+            CultureInfo cd = CultureInfo.CreateSpecificCulture(CultureInfo.CurrentCulture.Name);
+            cd.DateTimeFormat.ShortDatePattern = "dd/MM/yyyy";
+            Thread.CurrentThread.CurrentCulture = cd;
+        }
+    }
+}
